05ccb792bdb9fb57d8464a24d6e78b09ff118df3
Cyrus Wong <kwong582@my.bcit.ca>
2026-02-06 11:49:19 -0800

    5th Commit: Modified Mininet Script for possible debugging purpose

--- spectralwaste-segmentation/spectralwaste_segmentation/models/mininet/mininet.py
+++ spectralwaste-segmentation/spectralwaste_segmentation/models/mininet/mininet.py
@@ -58,7 +58,10 @@ class Conv2dBatchNorm(nn.Module):
     """
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, dilation=1, bias=True):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, 1, dilation, 1, bias)
+
+        #print("in_channels type = ", type(in_channels))
+        
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=1, dilation=dilation, groups=1, bias=bias)
         self.bn = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
